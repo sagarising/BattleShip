aa17e0903f110acc25f921bfb8993dbe57b74380
Sagar <sagarma@thoughtworks.com>
2015-11-17 19:41:33 +0530

    added todo.odt

--- Test/battleShipTest.js
+++ Test/battleShipTest.js
@@ -11,22 +11,33 @@ describe('Ship',function(){
 		var ship = new lib.Ship(3);
 		expect(ship).to.be.a('object');
 	});
-	it('returned ship must have only desired fields',function(){
+	it('returned ship must have only desired fields with null values',function(){
 		var ship1 = new lib.Ship(3); 
-		expect(ship1).to.have.all.keys(['a','b','c']);
+		expect(ship1).to.have.all.keys({'a':null,'b':null,'c':null});
 	});
 });
 
 describe('isAllowed',function(){
 	it('should check if the firstpoint is valid for placing ship aligned vertical',function(){
 		var ship = new lib.Ship(5);
-		assert.equal(true,lib.isAllowed(ship,"vertical",{row:"B",column:9},lib.grid)); 
-		assert.equal(false,lib.isAllowed(ship,"vertical",{row:"I",column:9},lib.grid)); 
+		assert.equal(true,lib.isAllowed(ship,'vertical',{row:'B',column:9},new lib.gridCreater)); 
+		assert.equal(false,lib.isAllowed(ship,'vertical',{row:'I',column:9},new lib.gridCreater)); 
 	});
 	it('should check if the firstpoint is valid for placing ship aligned horizontal',function(){
 		var ship = new lib.Ship(5);
-		assert.equal(true,lib.isAllowed(ship,"vertical",{row:"B",column:9},lib.grid)); 
-		assert.equal(false,lib.isAllowed(ship,"vertical",{row:"I",column:9},lib.grid)); 
+		assert.equal(true,lib.isAllowed(ship,'vertical',{row:'B',column:9},new lib.gridCreater)); 
+		assert.equal(false,lib.isAllowed(ship,'vertical',{row:'I',column:9},new lib.gridCreater)); 
 	});
 });
+
+describe('isUsedCoordinate',function(){
+	it('returns true if coordinate is used by other ship');
+	it('returns false if coordinate is not used by any other ship');
+});
+
+describe('positionShip',function(){
+	it('fills the coordinates of the ship object');
+	it('returns error if the ship cannot be placed on used coordinate');
+})
+
 
