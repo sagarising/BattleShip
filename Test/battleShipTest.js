99fcd868a4ae5350c0d4000e9356bd70bb315fd9
Sagar <sagarma@thoughtworks.com>
2015-11-17 17:09:22 +0530

    using mocha framework for testing

--- Test/battleShipTest.js
+++ Test/battleShipTest.js
@@ -1,10 +1,32 @@
 var assert = require('chai').assert;
+var expect = require('chai').expect;
 var lib = require('../battleShip.js').lib;
 var test = {};
 exports.test = test; 
 
-test["find out whether firstPoint selected is allowed or not"]=function(){
-	var ship = new lib.Ship(5)
-	assert.equal(true,isAllowed(ship,"vertical",{row:"B",column:9},lib.grid)); 
-};
+
+
+describe('Ship',function(){
+	it('should initialize ship object',function(){
+		var ship = new lib.Ship(3);
+		expect(ship).to.be.a('object');
+	});
+	it('returned ship must have only desired fields',function(){
+		var ship1 = new lib.Ship(3); 
+		expect(ship1).to.have.all.keys(['a','b','c']);
+	});
+});
+
+describe('isAllowed',function(){
+	it('should check if the firstpoint is valid for placing ship aligned vertical',function(){
+		var ship = new lib.Ship(5);
+		assert.equal(true,lib.isAllowed(ship,"vertical",{row:"B",column:9},lib.grid)); 
+		assert.equal(false,lib.isAllowed(ship,"vertical",{row:"I",column:9},lib.grid)); 
+	});
+	it('should check if the firstpoint is valid for placing ship aligned horizontal',function(){
+		var ship = new lib.Ship(5);
+		assert.equal(true,lib.isAllowed(ship,"vertical",{row:"B",column:9},lib.grid)); 
+		assert.equal(false,lib.isAllowed(ship,"vertical",{row:"I",column:9},lib.grid)); 
+	});
+});
 
