474e745d76f7cfb3134f3cfc88cbbec8945fc7b1
Sagar <sagarma@thoughtworks.com>
2016-01-05 10:57:59 +0530

    corrected bug in server.js as it was not getting controller.js file. Added npm-debug.log in .gitignore

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var controller = require('../lib/controller');
+var controller = require('./lib/controller');
 var server = http.createServer(controller);
 server.listen(3000);
 console.log("server listening on 3000");
