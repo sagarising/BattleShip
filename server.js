a39baa5fe258291d833fe4bb8918d9fb0c3af2b6
NABEEL <nabeel@thoughtworks.com>
2016-01-19 20:20:34 +0530

    edited server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 var controller = require('./lib/controller');
 var server = http.createServer(controller);
-server.listen(process.env.OPENSHIFT_NODEJS_PORT||process.env.PORT||5000||process.env.OPENSHIFT_NODEJS_IP);
+server.listen(process.env.OPENSHIFT_NODEJS_PORT||process.env.PORT||5000,process.env.OPENSHIFT_NODEJS_IP);
 console.log("server started");
