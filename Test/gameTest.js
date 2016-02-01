6e553c941b5d99ecbe212aadda1e7c861a401203
shibi <shibik@thoughtworks.com>
2016-02-01 09:13:30 +0530

    [shibi] - added test for canStartPlaying function in game.js
    What changes have you made?
    
    *
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- Test/gameTest.js
+++ Test/gameTest.js
@@ -1,19 +1,64 @@
 var assert = require('chai').assert;
+var expect = require('chai').expect;
 var Game = require('../lib/Game');
 var sinon = require('sinon');
 describe('Game',function(){
-	it('Should have one player as I push',function() {
+	describe('addPlayer',function(){
 		var game = new Game(1);
-		var player = {name:"Abhi"};
-		game.addPlayer(player);
-		assert.equal(JSON.stringify(game._players),JSON.stringify([{name:"Abhi"}]));
+		it('Should have one player as I push',function() {
+			var player = {name:"Abhi"};
+			game.addPlayer(player);
+			assert.equal(JSON.stringify(game._players),JSON.stringify([{name:"Abhi"}]));
+		});	
+		it('Should have two player as I again push',function(){
+			var player = {name:"Nabhi"};
+			game.addPlayer(player);
+			assert.equal(JSON.stringify(game._players),JSON.stringify([{name:"Abhi"},{name:"Nabhi"}]));
+		});
 	});
-	it('Should allow to play when both players are ready',function(){
-		var game = new Game(1);
-		game.addPlayer({name:"Abhi",isReady:true});
-		game.addPlayer({name:"Nabi",isReady:true});
-		assert.equal(game.canStartPlaying(),true);		
+	
+	describe("numberOfPlayers",function(){
+		it('Should return the number of players added',function(){
+			var game = new Game(1);
+			game.addPlayer({name:"Abhi"});
+			game.addPlayer({name:"Nbhi"});
+			var result = game.numberOfPlayers();
+			assert.equal(result,2);
+		});
 	});
+
+	describe("currentPlayerTurn",function(){
+		it('Should return turn of current player',function(){
+			var game = new Game(1);
+			game.addPlayer({name:"Abhi",turn:true});
+			game.addPlayer({name:"Nabi",turn:false});
+			var result = game.currentPlayerTurn("Abhi");
+			assert.equal(result,true);
+		});
+	});
+
+	describe("canStartPlaying",function(){
+		it('Should allow to play when both players are ready',function(){
+			var game = new Game(1);
+			game.addPlayer({name:"Abhi",isReady:true});
+			game.addPlayer({name:"Nabi",isReady:true});
+			assert.equal(game.canStartPlaying(),true);		
+		});
+		it('should return false when both players are not ready',function(){
+			var game = new Game(1);
+			game.addPlayer({name:"Abhi",isReady:false});
+			game.addPlayer({name:"Nabi",isReady:false});
+			var result = game.canStartPlaying();
+			expect(result).to.equal(false);
+		});
+		it('should return false when anyone is not ready and number of players is 1',function(){
+			var game = new Game(1);
+			game.addPlayer({name:"Nabi",isReady:true});
+			var result = game.canStartPlaying();
+			expect(result).to.equal(false);
+		});
+	});
+
 	it('Should give tell who is the current player is',function(){
 		var game = new Game(1);
 		game.addPlayer({name:"Abhi",isReady:true});
