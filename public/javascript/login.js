f5d1017fa4deb9547978fb10eafb00154777dd1b
Sagar <sagarma@thoughtworks.com>
2016-02-02 13:03:29 +0530

    removed helicopter sound and soundSwitch

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,19 +1,4 @@
-var sound_on_off = function() {
-     if($('#soundSwitch').text() == 'SOUND ON') {       
-        $('audio').each(function(){
-			this.play(); 
-    		this.currentTime = 0;
-		});
-       		$('#soundSwitch').text('SOUND OFF');
-     }
-     else {
-     	$('audio').each(function(){
-			this.pause(); 
-    		this.currentTime = 0;
-		});  
-        $('#soundSwitch').text('SOUND ON'); 
-     }
-};
+
 
 
 var login = function() {
