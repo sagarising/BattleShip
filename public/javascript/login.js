8bc08cfda76dbb1a38d1c678cf2e6a11282391f6
Sagar <sagarma@thoughtworks.com>
2016-02-03 01:21:30 +0530

    added instructions typed when user gets a page on login and ship deployment page

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,5 +1,17 @@
+var welcome = function(){
+	var str="Hey! Guess what, I was waiting for you! Enter your name and click on start button. If you want to play with your friend enter game id also.";
+	var length=str.length;
+	var index=0;
+	setInterval(function(){
+		if(index==length)
+			return;
+		var text = $("#welcome").text();
+		text+=str[index++];
+		$("#welcome").html(text);
+	},100);
+};
 
-
+setTimeout(welcome,500);
 
 var login = function() {
 	var Name = $("#name").val();
@@ -7,4 +19,4 @@ var login = function() {
 	$.post('login',{name: Name, gameId:GameId},function(data){
 		$('html').html(data);
 	});
-}
+};
