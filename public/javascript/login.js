a4bdc8ebdecb84c07b31f00a203be629e82cae2f
Jai Om Pandey <jaiomp@thoughtworks.com>
2016-02-02 12:17:54 +0530

    sound on and off button added to the login page

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,21 @@
+var sound_on_off = function() {
+     if($('#play').text() == 'SOUND ON') {       
+        $('audio').each(function(){
+			this.play(); 
+    		this.currentTime = 0;
+		});
+       		$('#play').text('SOUND OFF');
+     }
+     else {
+     	$('audio').each(function(){
+			this.pause(); 
+    		this.currentTime = 0;
+		});  
+        $('#play').text('SOUND ON'); 
+     }
+};
+
+
 var login = function() {
 	var Name = $("#name").val();
 	var GameId = $("#gameId").val();
