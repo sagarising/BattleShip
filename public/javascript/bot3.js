72c422fc40b6aeeee9a053e8884ac2d3b90d7b08
abhikur <thakura204@gmail.com>
2016-02-15 21:44:06 +0530

    Bot is ready to play the game

--- public/javascript/bot3.js
+++ public/javascript/bot3.js
@@ -6,8 +6,8 @@ var webdriver = require('selenium-webdriver'),
 var driver = new webdriver.Builder().forBrowser('chrome').build();
 
 var fillInformation = function(){
-	driver.findElement(By.name('name')).sendKeys('Robot');
-	// driver.findElement(By.name('gameId')).sendKeys('9994');
+	driver.findElement(By.name('name')).sendKeys(process.argv[2]);
+	driver.findElement(By.name('gameId')).sendKeys(process.argv[3]);
 	driver.findElement(By.tagName('button')).click();
 	return {place:placingShips};
 }
@@ -32,17 +32,35 @@ var placingShips = function() {
 	return {attack:attacker};
 }
 
+var allPoints = function(){
+	var points = [];
+	for (var i=65;i<75;i++){
+		for(var j=1;j<11;j++)
+			points.push(String.fromCharCode(i)+j);
+	};
+	return points;
+}
+
 var attacker = function() {
+	var points = allPoints(),i=0;
 	driver.wait(function(){
 		return driver.isElementPresent(webdriver.By.id("enemy"));
-	},10000).then(function(){
-		console.log(driver.getPageSource("Enemy's turn"));
+	},20000).then(function(){
+		driver.sleep(2000);
+		setInterval(function(){
+			driver.executeScript("var a = $('.turn').text(); return a").then(function(turn){
+				if(turn == "Your turn"){
+					console.log(turn,points[i]);
+					driver.findElement(By.css('#enemy #'+points[i++])).click();
+				}
+			})
+		},1000)
 	})
 
 }
 
-driver.get('http://battleship-step2015.rhcloud.com/');
+driver.get('http://localhost:5000/');
 fillInformation().place().attack();
-driver.quit();
+// driver.quit();
 
 // driver.quit();
