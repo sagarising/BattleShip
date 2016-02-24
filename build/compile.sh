3c7d5d3d1a7750f3e32a6dacc8aa97a3b910aaba
Sagar <sagarma@thoughtworks.com>
2016-02-24 13:06:14 +0530

    modified build files as suggested by abhikur.

--- build/compile.sh
+++ build/compile.sh
@@ -1,4 +1,2 @@
 rm -rf out/*.class
-javac -cp "src:lib/junit-4.12.jar" -d out test/GameTest.java
-javac -cp "src:lib/junit-4.12.jar" -d out test/PlayerTest.java
-javac -cp "src:lib/junit-4.12.jar" -d out test/ShipTest.java
+javac -cp "src:lib/junit-4.12.jar" -d out test/*.java
